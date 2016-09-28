b3d414865548bed4990fc7ce023643583a8f05b5
Ramkumar68 <ramkumr.kulriya@metacube.com>
2016-09-28 12:14:07 +0530

    initial

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+Book.destroy_all
+Book.create! [
+  {name: "Eloquent Ruby" , author: "Russ Olsen" },
+  {name: "Beginning Ruby", author: "Peter Cooper"},
+  {name: "Metaprogramming Ruby 2", author: "Paolo Perrotta" },
+  {name: "Design Patterns in Ruby", author: "Russ Olsen"},
+  {name: "The Ruby Programing Language", author: "David Flanagan"},
+]
+100.times { |index| Book.create! name: "Book#{index}", author: "Author#{index}"}
+eloquent = Book.find_by name: "Eloquent Ruby"
+eloquent.notes.create! [
+  {title: "Wow" ,note: "Greate book to learn Ruby"},
+  {title: "Funny", note: "Doesn't put you to sleep"}
+]
+reviewers = Reviewer.create! [
+  {name: "Joe", password: "abc123"},
+  {name: "Jim", password: "123abc"}
+]
+Book.all.each do |book|
+  book.reviewer =reviewers.sample
+  book.save!
+end
